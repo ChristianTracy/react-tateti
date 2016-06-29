6283477bb5572a6b6693011f0af0e75ff86c50c9
Christian Tracy <christian.tracy@nan-labs.com>
2016-06-29 14:30:54 -0300

    Get winner if exist method and success combinationns.

--- app/Tateti.js
+++ app/Tateti.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import GameCell from './components/GameCell';
+import _ from 'lodash';
+
+const successCombinations = [[0,1,2],[0,3,6],[1,4,7],[2,5,8],[3,4,5],[6,7,8],[0,4,8],[2,4,6]];
 
 class Tateti extends React.Component {
+
   constructor(props){
     super(props);
     this.state = {
@@ -9,34 +13,71 @@ class Tateti extends React.Component {
           {playedBy: '?'}, {playedBy: '?'}, {playedBy: '?'}, {playedBy: '?'}, {playedBy: '?'}, {playedBy: '?'},
           {playedBy: '?'}, {playedBy: '?'}, {playedBy: '?'}
       ],
-      currentPlayer: 'x'
+      currentPlayer: 'x',
+      winner: null
     }
   }
 
-  gameFinished(){
-    var equalFields = _.filter();
+  getWinnerIfExist(){
+    var count;
+    var winner = null;
+    var x = [];
+    var o = [];
+
+    _.forEach(this.state.cells, function(cell, idx){
+      if(cell.playedBy === 'x'){
+        x.push(idx);
+      }
+      else if (cell.playedBy === 'o'){
+        o.push(idx);
+      }
+    });
+
+    _.forEach(successCombinations, function(comb){
+      count = 0;
+      _.forEach(x, function(cell, idx){
+        if(_.includes(comb, cell)){
+          count++;
+        }
+      });
+      if(count === 3){
+        winner = 'x';
+      }
+      else{
+        count = 0;
+        _.forEach(o, function(cell, idx){
+          if(_.includes(comb, cell)){
+            count++;
+          }
+        });
+        if(count === 3){
+          winner = 'o';
+        }
+      }
+    });
+    return winner;
   }
 
 
   cellClicked(position){
-    if(this.state.cells[position].playedBy !== '?'){
-      return;
-    }
-    var self = this;
-    var cells = this.state.cells.map(function(cell, idx){
-      if(idx === position){
-        cell.playedBy = self.state.currentPlayer
+    if (this.state.winner === null){
+      if(this.state.cells[position].playedBy !== '?'){
+        return;
       }
-      return cell;
-    });
+      var self = this;
+      var cells = this.state.cells.map(function(cell, idx){
+        if(idx === position){
+          cell.playedBy = self.state.currentPlayer
+        }
+        return cell;
+      });
 
-    this.setState({
-      cells: cells,
-      currentPlayer: (this.state.currentPlayer === 'x') ? 'o' : 'x'
-    });
-
-
-
+      this.setState({
+        cells: cells,
+        currentPlayer: (this.state.currentPlayer === 'x') ? 'o' : 'x',
+        winner: this.getWinnerIfExist()
+      });
+    }
   }
 
   render(){
@@ -59,13 +100,15 @@ class Tateti extends React.Component {
             <GameCell handleCellClicked={this.cellClicked.bind(this)} player={this.state.cells[8]} position={8}/>
           </div>
         </div>
-        <div class="row text-center player-info">
+        <div className="row text-center player-info">
           <p>Next player: <span>{this.state.currentPlayer}</span></p>
+        </div>
+        <div className="row text-center winner-info">
+          <p>winner: <span>{this.state.winner}</span></p>
         </div>
       </div>
     )
   }
-
 
 }
 
